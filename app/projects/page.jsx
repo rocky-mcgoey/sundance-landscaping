632f9868491f67fc5dd0b42220f66d264fb37298
Rocky McGoey <mcgoeyrocky@gmail.com>
2025-12-16 00:28:41 -0700

    chore(projects): add bottom padding to grid and normalize project slugs

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -7,7 +7,7 @@ export default function ProjectsPage() {
     <BackgroundShell backgroundSrc="/images/background/Landscape-1.jpg">
       <div className="mx-auto max-w-6xl px-6 py-10">
         {/* Scrollable thumbnails area */}
-        <div className="h-[calc(100vh-160px)] overflow-y-auto pr-2">
+        <div className="h-[calc(100vh-160px)] overflow-y-auto pr-2 pb-16">
           <ProjectsGrid projects={projects} />
         </div>
       </div>
